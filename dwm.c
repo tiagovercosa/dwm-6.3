82c8faeb662c0ffffebbf1c09d9159bb7660da87
Tiago Verçosa <vercosa@protonmail.com>
2022-01-08 19:56:14 -0300

    patched rotate stack and tags

--- dwm.c
+++ dwm.c
@@ -182,6 +182,8 @@ static void detachstack(Client *c);
 static Monitor *dirtomon(int dir);
 static void drawbar(Monitor *m);
 static void drawbars(void);
+static void enqueue(Client *c);
+static void enqueuestack(Client *c);
 static void enternotify(XEvent *e);
 static void expose(XEvent *e);
 static void focus(Client *c);
@@ -212,6 +214,8 @@ static void resize(Client *c, int x, int y, int w, int h, int interact);
 static void resizeclient(Client *c, int x, int y, int w, int h);
 static void resizemouse(const Arg *arg);
 static void restack(Monitor *m);
+static void rotatetags(const Arg *arg);
+static void rotatestack(const Arg *arg);
 static void run(void);
 static void scan(void);
 static int sendevent(Client *c, Atom proto);
@@ -867,6 +871,28 @@ drawbars(void)
 }
 
 void
+enqueue(Client *c)
+{
+	Client *l;
+	for (l = c->mon->clients; l && l->next; l = l->next);
+	if (l) {
+		l->next = c;
+		c->next = NULL;
+	}
+}
+
+void
+enqueuestack(Client *c)
+{
+	Client *l;
+	for (l = c->mon->stack; l && l->snext; l = l->snext);
+	if (l) {
+		l->snext = c;
+		c->snext = NULL;
+	}
+}
+
+void
 enternotify(XEvent *e)
 {
 	Client *c;
@@ -1491,6 +1517,65 @@ restack(Monitor *m)
 	XSync(dpy, False);
 	while (XCheckMaskEvent(dpy, EnterWindowMask, &ev));
 }
+
+void
+rotatestack(const Arg *arg)
+{
+	Client *c = NULL, *f;
+
+	if (!selmon->sel)
+		return;
+	f = selmon->sel;
+	if (arg->i > 0) {
+		for (c = nexttiled(selmon->clients); c && nexttiled(c->next); c = nexttiled(c->next));
+		if (c){
+			detach(c);
+			attach(c);
+			detachstack(c);
+			attachstack(c);
+		}
+	} else {
+		if ((c = nexttiled(selmon->clients))){
+			detach(c);
+			enqueue(c);
+			detachstack(c);
+			enqueuestack(c);
+		}
+	}
+	if (c){
+		arrange(selmon);
+		//unfocus(f, 1);
+		focus(f);
+		restack(selmon);
+	}
+}
+
+void
+rotatetags(const Arg *arg)
+{
+	const int rot = abs(arg->i);
+	const unsigned int tagset = selmon->tagset[selmon->seltags];
+	unsigned int newtagset;
+
+	/* check the direction of the shift */
+	if (arg->i < 0) {
+		 /* shift tags right */
+		 newtagset = (tagset >> rot) | (tagset << (LENGTH(tags) - rot));
+	} else {
+		 /* shift tags left */
+		 newtagset = (tagset << rot) | (tagset >> (LENGTH(tags) - rot));
+	}
+
+	/* mask the tag bits */
+	newtagset &= TAGMASK;
+
+	if (newtagset) {
+		 selmon->tagset[selmon->seltags] = newtagset;
+		 focus(NULL);
+		 arrange(selmon);
+	}
+}
+
 
 void
 run(void)
