c286fc877a92884874848e6859b5fff78837598f
Tiago Verçosa <vercosa@protonmail.com>
2022-01-09 09:06:37 -0300

    remove patch centerfirstwindow

--- config.h
+++ config.h
@@ -1,12 +1,12 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 20;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 5;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 5;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 5;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 5;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
@@ -43,7 +43,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class				instance    title    tags mask     isfloating  isterminal  noswallow   CenterThisWindow?    monitor */
-	{ "st",       			NULL,       NULL,    0,             0,			1,          0,			 1,			        -1 },
+	{ "st",       			NULL,       NULL,    0,             0,			1,          0,			 0,			        -1 },
 	{ "Gimp",     			NULL,       NULL,    1 << 3,        0,          0,          1,  		 0,                 -1 },
 	{ "firefox",  			NULL,     "About Mozilla Firefox",  0,   1,		0,			1,           0,                 -1 },
 	{ "firefox",  			NULL,     "Library",      0,	    1,          1,          1,           0,                 -1 },
