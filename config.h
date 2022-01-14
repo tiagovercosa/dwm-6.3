c4ca6a7bbf7a5eca04f4fc4ce4dcadbd5e9e4b17
Tiago Verçosa <vercosa@protonmail.com>
2022-01-14 11:43:18 -0300

    my config

--- config.h
+++ config.h
@@ -4,18 +4,18 @@
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappih    = 5;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 5;       /* vert inner gap between windows */
+static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 5;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 5;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 /* static const char *fonts[]          = { "monospace:size=10" }; */
-static const char *fonts[]          = { "monospace:size=10:antialias=true:autohint=true",
+static const char *fonts[]          = { "Hack Nerd Font Mono:size=10:antialias=true:autohint=true",
                                         "Hack:size=8:antialias=true:autohint=true",
                                         "JoyPixels:size=10:antialias=true:autohint=true" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char dmenufont[]       = "Hack Nerd Font Mono:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -57,6 +57,7 @@ static const Rule rules[] = {
 	{ "Lxappearance",       NULL,     	NULL,    0,         	1,          0,          1,         -1 },
 	{ "qt5ct",              NULL,     	NULL,    0,         	1,          0,          1,         -1 },
 	{ "Pavucontrol",        NULL,     	NULL,    0,         	1,          0,          1,         -1 },
+	{ "Virt-manager",       NULL,     	NULL,    0,         	1,          0,          1,         -1 },
 	{ "Pcmanfm",			NULL,  "Execute File",    0,        1,          0,          1,         -1 },
 	{ "Nvidia-settings",    NULL,  "NVIDIA X Server Settings",  0,      1,      0,      1,         -1 },
 	{ NULL,					NULL,  "Event Tester", 0,			0,          0,          1,         -1 }, /* xev */
@@ -105,7 +106,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-b", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run",  "-p",  "Run:", "-b", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *tabtermcmd[]  = { "tabbed", "-r 2", "st", "-w", "''", NULL };
 
@@ -192,7 +193,7 @@ static Key keys[] = {
   	{ 0,               XF86XK_AudioStop,   	   spawn,          SHCMD("playerctl stop")},
 
   	 /* lockscreen with i3lock */
-  	{ MODKEY|ControlMask,			XK_l,      spawn,          SHCMD("i3lock-fancy")},
+  	{ MODKEY|ControlMask,			XK_l,      spawn,          SHCMD("slock")},
 	
 	/* Open App's*/
 	{	MODKEY|ShiftMask,			XK_f,	   spawn,		   SHCMD("firefox")},
@@ -201,7 +202,7 @@ static Key keys[] = {
 	/* Screenshot */
   	{MODKEY,           XK_Print,           spawn,          SHCMD("flameshot gui")},
   	{0,                XK_Print,           spawn,          SHCMD("flameshot full -c")},
-  	{ShiftMask,        XK_Print,           spawn,          SHCMD("flameshot full -p /home/tiago/Pictures/screenshots")}
+  	{ShiftMask,        XK_Print,           spawn,          SHCMD("flameshot full -p $HOME/img/screenshots")}
   	/* {ControlMask,      XK_Print,           spawn,          SHCMD("scrot -u /home/tiago/Pictures/screenshots/`date +%Y-%m-%d_%H:%M:%S`.png")} */
 };
 
